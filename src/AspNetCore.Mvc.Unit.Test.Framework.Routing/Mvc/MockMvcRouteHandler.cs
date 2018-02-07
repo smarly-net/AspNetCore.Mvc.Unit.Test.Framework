f16dad5474fece80a8f7488a43bccc71c1bbf306
denis.dmitriev <d.dzmitryieu@godeltech.com>
2018-02-07 17:17:57 +0300

    route handler

--- src/AspNetCore.Mvc.Unit.Test.Framework.Routing/Mvc/MockMvcRouteHandler.cs
+++ src/AspNetCore.Mvc.Unit.Test.Framework.Routing/Mvc/MockMvcRouteHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -12,19 +13,22 @@ namespace AspNetCore.Mvc.Unit.Test.Framework.Routing.Mvc
 {
     public class MockMvcRouteHandler : MvcRouteHandler, IRouter
     {
+        private readonly IActionInvokerFactory _actionInvokerFactory;
         private readonly IActionSelector _actionSelector;
+        private readonly IActionContextAccessor _actionContextAccessor;
 
-        public MockMvcRouteHandler(IActionInvokerFactory actionInvokerFactory, IActionSelector actionSelector, DiagnosticSource diagnosticSource, ILoggerFactory loggerFactory) : base(actionInvokerFactory, actionSelector, diagnosticSource, loggerFactory)
+        public MockMvcRouteHandler(IActionInvokerFactory actionInvokerFactory, IActionSelector actionSelector, DiagnosticSource diagnosticSource, ILoggerFactory loggerFactory) : this(actionInvokerFactory, actionSelector, diagnosticSource, loggerFactory, actionContextAccessor: null)
         {
-            _actionSelector = actionSelector;
         }
 
         public MockMvcRouteHandler(IActionInvokerFactory actionInvokerFactory, IActionSelector actionSelector, DiagnosticSource diagnosticSource, ILoggerFactory loggerFactory, IActionContextAccessor actionContextAccessor) : base(actionInvokerFactory, actionSelector, diagnosticSource, loggerFactory, actionContextAccessor)
         {
+            _actionInvokerFactory = actionInvokerFactory;
             _actionSelector = actionSelector;
+            _actionContextAccessor = actionContextAccessor;
         }
 
-        public async Task RouteAsync(RouteContext context)
+        async Task IRouter.RouteAsync(RouteContext context)
         {
             await base.RouteAsync(context);
 
@@ -33,14 +37,31 @@ namespace AspNetCore.Mvc.Unit.Test.Framework.Routing.Mvc
                 var candidates = _actionSelector.SelectCandidates(context);
                 var actionDescriptor = _actionSelector.SelectBestCandidate(context, candidates) as ControllerActionDescriptor;
 
+
+
                 if (actionDescriptor == null)
                 {
                     throw new NotImplementedException();
                 }
 
-                actionDescriptor.
-
                 var routeData = httpContext.GetRouteData();
+
+                var actionContext = new ActionContext(context.HttpContext, routeData, actionDescriptor);
+                if (_actionContextAccessor != null)
+                {
+                    _actionContextAccessor.ActionContext = actionContext;
+                }
+
+                var invoker = _actionInvokerFactory.CreateInvoker(actionContext);
+                if (invoker == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return invoker.InvokeAsync();
+
+
+
                 return Task.CompletedTask;
             };
 
